c90e916b42e2f246cf2b17d0730c2e9c19fbd222
kot <bafomet2016vitt@gmail.com>
2023-07-31 07:04:12 +0300

    manager #ok

--- src/db_manager.py
+++ src/db_manager.py
@@ -34,37 +34,69 @@ class BaseManager(ABC):
         """Получает список всех вакансий, у которых зарплата выше средней по всем
         вакансиям."""
     @abstractmethod
-    def get_vacancies_with_keyword(self):
+    def get_vacancies_with_keyword(self, word: str):
         """Получает список всех вакансий, в названии которых содержатся переданные
         в метод слова, например “python”."""
 
 
 class DBManager(BaseManager):
     """
-    Класс, который подключается к БД Postgres
+    Работа с БД Postgres
     """
 
     def get_companies_and_vacancies_count(self):
         """Получает список всех компаний и количество вакансий
         у каждой компании."""
+        cur = self.conn.cursor()
+        cur.execute('''SELECT employer.name, COUNT(vacancy.name) 
+        FROM employer JOIN vacancy USING (employer_id)
+        GROUP BY employer.name
+        ORDER BY employer.name ASC''')
+        return cur.fetchall()
 
     def get_all_vacancies(self):
         """Получает список всех вакансий с указанием названия компании, названия
         вакансии и зарплаты и ссылки на вакансию."""
+        cur = self.conn.cursor()
+        cur.execute('''SELECT employer.name, vacancy.name, vacancy.salary, vacancy.alternate_url 
+        FROM employer JOIN vacancy USING (employer_id)
+        ORDER BY employer.name ASC''')
+        data = cur.fetchall()
+        cur.close()
+        return data
 
     def get_avg_salary(self):
         """Получает среднюю зарплату по вакансиям."""
+        cur = self.conn.cursor()
+        cur.execute('''SELECT AVG(salary)
+        FROM vacancy WHERE salary <> 0''')
+        data = cur.fetchall()
+        cur.close()
+        return data
 
     def get_vacancies_with_higher_salary(self):
         """Получает список всех вакансий, у которых зарплата выше средней по всем
         вакансиям."""
+        cur = self.conn.cursor()
+        cur.execute('''SELECT name, salary, alternate_url FROM vacancy
+        WHERE salary > (SELECT AVG(salary) FROM vacancy WHERE salary <> 0)''')
+        data = cur.fetchall()
+        cur.close()
+        return data
 
-    def get_vacancies_with_keyword(self):
+    def get_vacancies_with_keyword(self, word: str):
         """Получает список всех вакансий, в названии которых содержатся переданные
         в метод слова, например “python”."""
+        word = '%' + word + '%'
+        cur = self.conn.cursor()
+        cur.execute("""SELECT name, salary, alternate_url
+        FROM vacancy WHERE name ILIKE %s""", (word,))
+        data = cur.fetchall()
+        cur.close()
+        return data
 
     def create_table(self):
-        """Выполняет первые три запроса из queries.sql"""
+        """Выполняет первые три запроса из queries.sql, тем самым создает таблицы"""
         cur = self.conn.cursor()
         with open('queries.sql') as file:
             for i in range(3):
@@ -78,31 +110,38 @@ class DBManager(BaseManager):
     def vacancy_to_db(self, vacancies: list[Vacancy]):
         """Загружает вакансии в базу"""
         cur = self.conn.cursor()
+        area_id = []
+        employer_id = []
         for i in vacancies:
 
-            if not i.employer.id:  # Увы, иногда Api отдаёт странные вещи..
+            if not i.employer.id:  # Увы, иногда Api отдаёт странные вещи...
                 continue
 
-            cur.execute('INSERT INTO area (id, name, url) VALUES (%s, %s, %s) ON CONFLICT DO NOTHING;',
-                        (i.area.id, i.area.name, i.area.url))
+            if i.area.id not in area_id:  # Чтобы не делать лишних запросов
+                area_id.append(i.area.id)
 
+                cur.execute('INSERT INTO area (area_id, name, url) VALUES (%s, %s, %s) ON CONFLICT DO NOTHING;',
+                            (i.area.id, i.area.name, i.area.url))
 
-            cur.execute('''INSERT INTO employer 
-            (id, name, alternate_url, vacancies_url, accredited_it_employer, trusted, url) 
-            VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING;''',
-                        (i.employer.id,
-                         i.employer.name,
-                         i.employer.alternate_url,
-                         i.employer.vacancies_url,
-                         i.employer.accredited_it_employer,
-                         i.employer.trusted,
-                         i.employer.url))
+            if i.employer.id not in employer_id:  # Чтобы не делать лишних запросов
+                employer_id.append(i.employer.id)
+
+                cur.execute('''INSERT INTO employer 
+                (employer_id, name, alternate_url, vacancies_url, accredited_it_employer, trusted, url) 
+                VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING;''',
+                            (i.employer.id,
+                             i.employer.name,
+                             i.employer.alternate_url,
+                             i.employer.vacancies_url,
+                             i.employer.accredited_it_employer,
+                             i.employer.trusted,
+                             i.employer.url))
 
             salary = self.calc_salary(i.salary) if i.salary else 0
-            snippet = self.summ_snippet(i.snippet) if i.snippet else ''
+            snippet = self.join_snippet(i.snippet) if i.snippet else ''
 
             cur.execute('''INSERT INTO vacancy 
-            (id, name, alternate_url, created_at, salary, snippet , apply_alternate_url, area_id, employer_id)
+            (vacancy_id, name, alternate_url, created_at, salary, snippet , apply_alternate_url, area_id, employer_id)
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING;''',
                         (i.id,
                          i.name,
@@ -113,6 +152,7 @@ class DBManager(BaseManager):
                          i.apply_alternate_url,
                          i.area.id,
                          i.employer.id))
+        cur.close()
 
     @staticmethod
     def calc_salary(salary: dict) -> int:
@@ -127,7 +167,7 @@ class DBManager(BaseManager):
         return int(sal_sum / count)
 
     @staticmethod
-    def summ_snippet(snippet: 'Snippet') -> str:
+    def join_snippet(snippet: 'Snippet') -> str:
         data = ''
         if snippet.requirement:
             data += snippet.requirement
