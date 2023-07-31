75a0ec848c766ec3ce10c12a94ab57e8af2b472c
kot <bafomet2016vitt@gmail.com>
2023-07-31 11:07:49 +0300

    RC v1.0

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,6 @@
 from src.api import VacancyApi
 from src.models import VacancyBase
+from src.area_selector import AreaSelector
 
 
 def vacancy_collector(api: 'VacancyApi', text='Python', area_id=None, count=20) -> list:
@@ -32,3 +33,30 @@ def vacancy_collector(api: 'VacancyApi', text='Python', area_id=None, count=20) 
         vacancies.extend(model.items)
 
     return vacancies
+
+
+def load_api():
+    """Загружает данные из Api"""
+    area_id_ = None
+
+    if not input('Задать местность для поиска Enter-да/0-нет '):
+        area_id_ = AreaSelector().get()
+
+    word = input('Введите ключевое слово для поиска: ')
+
+    while 1:
+
+        count_vac = input('Введите количество вакансий от 1 до 2000')
+
+        if not count_vac.isdigit():
+            print('Некорректный ввод')
+            continue
+        count_vac = int(count_vac)
+
+        if not 0 < count_vac <= 2000:
+            print('Некорректный ввод')
+            continue
+
+        break
+
+    return vacancy_collector(VacancyApi(), word, area_id_, count_vac)
--- src/main.py
+++ src/main.py
@@ -1,29 +1,54 @@
-from src.functions import vacancy_collector
-from src.api import VacancyApi, EmployerApi
-from src.models import EmployerFull
+from src.functions import load_api
 from src.db_manager import DBManager
 
+data = ['1 - Загрузить данные из hh в БД',
+        '2 - Вывести все компании и количество их вакансий',
+        '3 - Вывести список всех вакансий',
+        '4 - Вывести среднюю зарплату по вакансиям',
+        '5 - Вывести вакансии, у которых зарплата выше средней',
+        '6 - Вывести из БД вакансии по ключевому слову',
+        '7 - Проинициализировать БД (если первый запуск)',
+        '10 - выйти из программы',
+        'Введите число: ']
 
-def api():
-    hh = VacancyApi()
-    data = vacancy_collector(hh, text='Python', area_id=1, count=1570)
+manager = DBManager()
 
-    return data
+while 1:
+    action = input('\n'.join(data))
 
+    if action == '1':
+        vacs = load_api()
+        manager.vacancy_to_db(vacs)
+        print('OK')
 
-def api2():
-    hh = EmployerApi()
-    hh.employer_id = '4768936'
+    if action == '2':
+        data = manager.get_companies_and_vacancies_count()
+        for i in data:
+            print(*i)
 
-    data = hh.get()
+    if action == '3':
+        data = manager.get_all_vacancies()
+        for i in data:
+            print(*i)
 
-    data2 = EmployerFull.model_validate_json(data)
-    for i in data2:
-        print(i)
+    if action == '4':
+        data = manager.get_avg_salary()
+        for i in data:
+            print(*i)
 
+    if action == '5':
+        data = manager.get_vacancies_with_higher_salary()
+        for i in data:
+            print(*i)
 
-dat = api()
-man = DBManager()
-man.create_table()
-input()
-man.vacancy_to_db(dat)
+    if action == '6':
+        data = manager.get_vacancies_with_keyword(input('Слово: '))
+        for i in data:
+            print(*i)
+
+    if action == '7':
+        data = manager.create_table()
+
+    if action == '10':
+        break
+
